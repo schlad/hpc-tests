4815a763efd47b2cbc3c8cffe50254c31562bf12
Sebastian Chlad <schlad@suse.de>
2019-06-26 14:41:40 +0200

    POC: Initial .sh for executing all available tests
    The idea is to have some shell based tests which could be easily
    used using terraform/libvirt but also could be used from within
    openQA-provisioned HPC cluster

---
+++ openqa_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This is a POC for running simple HPC tests against the HPC cluster
+# provisioned by openQA
+#
+
+run_basic () {
+    echo "Basic tests start"
+}
+
+run_nfs () {
+}
+
+run_db () {
+}
